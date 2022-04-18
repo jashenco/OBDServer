f03e621e26e41f37e1b1d974eb1f0665a1876d4d
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2022-04-18 15:23:08 +0200

    test push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World! This will be the OBD Server.</p>"
+    return "<p>Hello, World! This will be the OBD Server. Test Push.</p>"
 
 if __name__ == "__main__": 
     app.run(debug=True, host='0.0.0.0')
