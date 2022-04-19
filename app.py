7792cf90e91e4dd28d9844c691ad94f98e7eccf1
jaspervangent69 <57948820+jaspervangent69@users.noreply.github.com>
2022-04-19 12:33:55 +0200

    database

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import datetime
+import json
+
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_file
+db = SQLAlchemy(app)
+
+class JsonModel(object):
+    def as_dict(self):
+       return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+class CarStatistic(db.Model, JsonModel):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String(100), unique=False, nullable=False)
+    value = db.Column(db.String(100), unique=False)
+    date = db.Column(db.DateTime, unique=False)
+
+    def __repr__(self):
+        return f'[{self.id}, {self.key}, {self.value}, {self.date}]'
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World! This will be the OBD Server. Test Push. Watchtower Test. Test 2.</p>"
+def home():
+    return "OBD App"
 
-if __name__ == "__main__": 
-    app.run(debug=True, host='0.0.0.0')
+@app.route("/stats", methods=['GET', 'POST'])
+def addstat():
+    if request.method == 'POST':
+        content = request.json
+
+        key = content['key']
+        value = content['value']
+        date = datetime.datetime.now()
+
+        newCarStatistic = CarStatistic(key=key, value=value, date=date)
+
+        db.session.add(newCarStatistic)
+        db.session.commit()
+
+        return "data received"
+
+    elif request.method == 'GET':
+        return json.dumps([s.as_dict() for s in CarStatistic.query.all()], default=str)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
